7609d85a2f2f64fc4d122ff22b4d9b7538ade227
Lfifolato <ldalri1999@gmail.com.br>
2020-06-11 01:20:33 -0300

    teste 01

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Todo'),
+      home: MyHomePage(title: 'Flutter teste'),
     );
   }
 }
